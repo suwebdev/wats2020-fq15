28478f43976c44b003c2003352ec796fe86f47ac
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2015-10-25 20:15:59 -0700

    added 'save poem' feature to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(document).on('ready', function() {
 
+	$('.add-favorite').on('click', function(event) {
+		
+		// Grab poem information
+		var poemTitle = $(event.target).parent().parent().find('.media-heading').text();
+		var poemAuthor = $(event.target).parent().parent().find('.author').text();
+		var poemURL = $(event.target).parent().parent().data('url');
+
+		// Print object values to make sure they are correct
+		console.log(poemTitle);
+		console.log(poemAuthor);
+		console.log(poemURL);
+
+		// Create poem object to save to localstorage
+		var fav = {
+			'title': poemTitle,
+			'author': poemAuthor,
+			'url': poemURL
+		};
+
+		localStorage.setItem('fav1', JSON.stringify(fav));
+		console.log(JSON.parse(localStorage.getItem('fav1')));
+	});
+});
